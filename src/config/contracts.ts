5b9a03856236f66c497233ce62d19e56b0ad5d96
iggynore <iggynore@mtrkd.com>
2025-10-22 11:52:28 -0300

    New revenueFacet address

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -20,7 +20,7 @@ export const CONTRACTS = {
   VaultFacet: "0xd6157e381ec5c1d06aca995fa77c5d0a388ff512",
   BondingCurveFacet: "0x400b47355be05bbe21f5660016b50b93ab6c9267",
   LabPassFacet: "0x799cb349e8feec8ce0155abe0a2f258307a66834",
-  RevenueFacet: "0xf74a1adb71ab5769836eafac607fc5c31ee83488",
+  RevenueFacet: "0xbe14097af4ad6f9ffd9ec39b404121ab44a5b112",
   TreasuryFacet: "0x65c38321d60b3e2fdbf30a474ff7063eadc9f509",
   DataValidationFacet: "0xb607b954ed414bcdba321a02f487991518a795c0",
   CredentialFacet: "0x3ffab9fa9568c68bf60c88891f238556f8d99387",
